db67ab4e3bf7d7e49c6a427544f841156e8fd60f
Dorina Mihai <dorinamc@outlook.com>
2018-09-14 18:10:49 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var adjective = prompt("Please type an adjective");
+var verb = prompt("Please type a verb");
+var noun = prompt("Please type a noun");
+alert("All done. Ready for the message?");
+var message = "<h2>There once was a " + adjective + " programmer who wanted to " + verb + " the " + noun + ".</h2>";
+document.write(message);
